18d2d440a72c5ce6554e1fb368ac025062fd83ff
smarchevsky <staseg1987@gmail.com>
2023-04-21 00:06:18 +0300

    Fill RGBA image with constant color

--- src/graphics/image.h
+++ src/graphics/image.h
@@ -3,15 +3,19 @@
 #include "helper_general.h"
 #include <filesystem>
 #include <glm/vec2.hpp>
+#include <glm/vec4.hpp>
 
 class Image : NoCopy<Image> {
 public:
     Image();
     Image(const std::filesystem::path& path) { load(path); }
+    Image(glm::ivec2 size, glm::ivec4 color) { fill(size, color); }
     Image(Image&& rhs);
     ~Image();
 
     void load(const std::filesystem::path& path);
+    void fill(glm::ivec2 size, glm::ivec4 color);
+    void clear();
     bool isValid() const;
 
 private:
