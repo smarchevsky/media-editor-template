06e5075edaf66e876448b2b826fc36248c966dce
smarchevsky <staseg1987@gmail.com>
2023-04-21 01:51:57 +0300

    Set filtering as separate function

--- src/graphics/gltexture.cpp
+++ src/graphics/gltexture.cpp
@@ -4,6 +4,8 @@
 #include <SDL2/SDL_opengl.h>
 #include <iostream>
 
+uint32_t GLTexture::s_currentBindedTexture = 0;
+
 static int getGLTextureFormatInternal(GLTexture::Format format)
 {
     switch (format) {
@@ -31,7 +33,15 @@ static int getGLTextureFormatExternal(int nrChannels)
 
 GLTexture::~GLTexture()
 {
-    glDeleteTextures(1, &m_textureHandle);
+    clear();
+}
+
+bool GLTexture::createEmpty(glm::ivec2 size)
+{
+    if (size.x <= 0 || size.y <= 0)
+        return false;
+
+    return true;
 }
 
 bool GLTexture::fromImage(const Image& img)
@@ -49,53 +59,105 @@ bool GLTexture::fromImage(const Image& img)
     if (!externalTextureFormat)
         return false;
 
+    clear();
+
     const GLenum texelType = GL_UNSIGNED_BYTE; // stbImage always gives uint8 image format
 
     glGenTextures(1, &m_textureHandle);
-
     bind();
+
+    glTexImage2D(GL_TEXTURE_2D, 0, internalTextureFormat, img.m_size.x, img.m_size.y,
+        0 /* border? */, externalTextureFormat, texelType, img.m_data);
+
     // set the texture wrapping parameters
-    setOutOfBoundsBehavior(OutOfBoundsBehavior::Repeat);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-
-    glTexImage2D(GL_TEXTURE_2D, 0,
-        internalTextureFormat,
-        img.m_size.x, img.m_size.y,
-        0 /* border? */,
-        externalTextureFormat, texelType, img.m_data);
-
-    glHint(GL_GENERATE_MIPMAP_HINT, GL_NICEST /*GL_FASTEST*/);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    setWrapping(Wrapping::Repeat);
+    setFiltering(Filtering::LinearMipmap);
     return true;
 }
 
-bool GLTexture::setOutOfBoundsBehavior(OutOfBoundsBehavior behavior)
+void GLTexture::clear()
 {
+    if (m_textureHandle) {
+        glDeleteTextures(1, &m_textureHandle);
+        m_textureHandle = 0;
+    }
+}
+
+bool GLTexture::setWrapping(Wrapping behavior)
+{
+    if (!m_textureHandle) {
+        LOGE("No texture handle");
+        return false;
+    }
+
     int GLBehaviorHandle {};
     switch (behavior) {
-    case OutOfBoundsBehavior::Repeat: {
+    case Wrapping::Repeat: {
         GLBehaviorHandle = GL_REPEAT;
     } break;
-    case OutOfBoundsBehavior::MirrorRepeat: {
+    case Wrapping::MirrorRepeat: {
         GLBehaviorHandle = GL_MIRRORED_REPEAT;
     } break;
-    case OutOfBoundsBehavior::ClampEdge: {
+    case Wrapping::ClampEdge: {
         GLBehaviorHandle = GL_CLAMP_TO_EDGE;
     } break;
-    case OutOfBoundsBehavior::ClampBorder: {
+    case Wrapping::ClampBorder: {
         GLBehaviorHandle = GL_CLAMP_TO_BORDER;
     } break;
     default:
-        LOGE("Unsuppoerted OutOfBoundsBehavior type");
+        LOGE("Unsuppoerted wrapping type");
         return false;
     }
-    // bind ?
+
+    bind();
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GLBehaviorHandle);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GLBehaviorHandle);
     return true;
 }
 
-void GLTexture::bind() { glBindTexture(GL_TEXTURE_2D, m_textureHandle); }
+bool GLTexture::setFiltering(Filtering filtering)
+{
+    if (!m_textureHandle) {
+        LOGE("No texture handle");
+        return false;
+    }
+
+    int minFilter, magFilter;
+    switch (filtering) {
+    case Filtering::Nearset: {
+        minFilter = magFilter = GL_NEAREST;
+    } break;
+    case Filtering::Linear: {
+        minFilter = magFilter = GL_LINEAR;
+    } break;
+    case Filtering::LinearMipmap: {
+        minFilter = GL_LINEAR_MIPMAP_LINEAR;
+        magFilter = GL_LINEAR;
+
+        // glHint(GL_GENERATE_MIPMAP_HINT, GL_NICEST /*GL_FASTEST*/);
+        if (m_textureHandle)
+            glGenerateMipmap(GL_TEXTURE_2D);
+    } break;
+    default:
+        LOGE("Unsupportering texture filtering type");
+        return false;
+    }
+    m_filtering = filtering;
+    bind();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+    return true;
+}
+
+void GLTexture::bind()
+{
+// #define ALWAYS_BIND_MESH
+#ifdef ALWAYS_BIND_MESH
+    glBindTexture(GL_TEXTURE_2D, m_textureHandle);
+#else
+    if (s_currentBindedTexture != m_textureHandle) {
+        s_currentBindedTexture = m_textureHandle;
+        glBindTexture(GL_TEXTURE_2D, m_textureHandle);
+    }
+#endif
+}
