35eacea166f554f4478415d3331c3d68a8104e58
smarchevsky <staseg1987@gmail.com>
2023-04-28 16:17:35 +0300

    Simplify shaders and rendering

--- src/graphics/gl_rendermanager.h
+++ src/graphics/gl_rendermanager.h
@@ -7,8 +7,15 @@
 
 class GLRenderManager {
 public:
-    void draw(GLFrameBufferBase* frameBuffer, CameraBase* camera, const std::vector<std::unique_ptr<DrawableBase>>& drawable);
-    void draw(GLFrameBufferBase* frameBuffer, CameraBase* camera, DrawableBase* drawable);
+    void draw(GLShader &shader,
+        GLFrameBufferBase &frameBuffer,
+        CameraBase* camera,
+        const std::vector<std::unique_ptr<DrawableBase>>& drawable);
+
+    void draw(GLShader &shader,
+        GLFrameBufferBase &frameBuffer,
+        CameraBase* camera,
+        DrawableBase* drawable);
 };
 
 #endif // GL_RENDERMANAGER_H
