a1e193096bf05befe8982ef1826e4a260480742e
smarchevsky <staseg1987@gmail.com>
2023-04-29 12:46:03 +0300

    Camera refactoring, added perspective camera

--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -3,25 +3,52 @@
 
 #include "gl_shader.h"
 
-CameraBase::CameraBase()
-{
-}
+////////////////////////// CAMERA ORTHO //////////////////////////////
 
-CameraOrtho::CameraOrtho()
+const glm::mat4& CameraOrtho::getView()
 {
-}
-
-void CameraOrtho::updateUniforms(GLShader* shader)
-{
-    if (m_isViewProjectionDirty) {
-        m_isViewProjectionDirty = false;
+    if (m_viewDirty) {
+        m_viewDirty = false;
         glm::vec2 scale = m_viewportSize * m_scaleMultiplier;
         glm::vec2 p0 = m_posWorld - scale * 0.5f;
         glm::vec2 p1 = m_posWorld + scale * 0.5f;
         // m_viewProjection = glm::ortho(p0.x, p1.x, p0.y, p1.y, -1.f, 1.f);
-        m_viewProjection = glm::ortho(p0.x, p1.x, p1.y, p0.y, -1.f, 1.f);
+        m_matView = glm::ortho(p0.x, p1.x, p1.y, p0.y, -1.f, 1.f);
     }
+    return m_matView;
+}
+
+void CameraOrtho::updateUniforms(GLShader* shader)
+{
     if (shader) {
-        shader->setUniform("matView", m_viewProjection);
+        shader->setUniform("matView", getView());
+    }
+}
+
+////////////////////// CAMERA PERSPECTIVE //////////////////////////////
+
+const glm::mat4& CameraPerspective::getView()
+{
+    if (m_viewDirty) {
+        m_matView = glm::lookAt(m_pos, m_aim, m_up);
+        m_viewDirty = false;
+    }
+    return m_matView;
+}
+
+const glm::mat4& CameraPerspective::getProjection()
+{
+    if (m_projectionDirty) {
+        m_matProjection = glm::perspective(m_fov, m_ar, m_near, m_far);
+        m_projectionDirty = false;
+    }
+    return m_matProjection;
+}
+
+void CameraPerspective::updateUniforms(GLShader* shader)
+{
+    if (shader) {
+        shader->setUniform("matView", getView());
+        shader->setUniform("matProjection", getProjection());
     }
 }
--- src/graphics/camera.h
+++ src/graphics/camera.h
@@ -4,54 +4,92 @@
 #include <glm/mat4x4.hpp>
 #include <glm/vec2.hpp>
 #include <string>
-#include <unordered_map>
 
 class GLShader;
 
 class CameraBase {
-protected:
-    glm::mat4 m_viewProjection;
-
 public:
-    CameraBase();
     virtual ~CameraBase() = default;
-    virtual void updateUniforms(GLShader* shader) { }
+    virtual void updateUniforms(GLShader* shader) = 0;
 };
 
-//////////////// ORTHO CHAMERA //////////////////////
+////////////////////////// CAMERA ORTHO //////////////////////////////
 
 class CameraOrtho : public CameraBase {
+protected:
+    glm::mat4 m_matView = glm::mat4(1);
 
 private:
     glm::vec2 m_posWorld = glm::vec2(0);
     glm::vec2 m_viewportSize = glm::vec2(1000, 1000);
     float m_scaleMultiplier = 0.003f;
 
-    bool m_isViewProjectionDirty = true;
+    bool m_viewDirty = true;
 
 public:
-    CameraOrtho();
-
-    // clang-format off
-    void setScale(float scale)            { m_scaleMultiplier = scale,        m_isViewProjectionDirty = true; }
-    void multiplyScale(float scaleOffset) { m_scaleMultiplier *= scaleOffset, m_isViewProjectionDirty = true; }
+    void setScale(float scale) { m_scaleMultiplier = scale, m_viewDirty = true; }
+    void multiplyScale(float scaleOffset) { m_scaleMultiplier *= scaleOffset, m_viewDirty = true; }
     void multiplyScaleOffseted(float scaleFactor, glm::vec2 viewSpaceOffset)
     {
-        glm::vec2 mousePosOffset = (viewSpaceOffset - 0.5f * m_viewportSize) * m_scaleMultiplier;
+        glm::vec2 mousePosOffset = (viewSpaceOffset - 0.5f * m_viewportSize)
+            * m_scaleMultiplier;
         m_posWorld += mousePosOffset * (1 - scaleFactor);
         multiplyScale(scaleFactor);
     }
-    void setViewportSize(glm::vec2 vs)    { m_viewportSize = vs,              m_isViewProjectionDirty = true; }
-    void setPosition_WS(glm::vec2 pos)    { m_posWorld = pos,                 m_isViewProjectionDirty = true; }
-    void addOffset_WS(glm::vec2 offset)   { m_posWorld += offset,             m_isViewProjectionDirty = true; }
 
-    void addOffset_View(glm::vec2 offset) {addOffset_WS(offset * m_scaleMultiplier); }
+    void setViewportSize(glm::vec2 vs) { m_viewportSize = vs, m_viewDirty = true; }
+    void setPosition_WS(glm::vec2 pos) { m_posWorld = pos, m_viewDirty = true; }
+    void addOffset_WS(glm::vec2 offset) { m_posWorld += offset, m_viewDirty = true; }
+    void addOffset_View(glm::vec2 offset) { addOffset_WS(offset * m_scaleMultiplier); }
 
-    glm::vec2 getPos()        const       { return m_posWorld; }
-    float getScale()          const       { return m_scaleMultiplier; }
-    // clang-format on
+    glm::vec2 getPos() const { return m_posWorld; }
+    float getScale() const { return m_scaleMultiplier; }
+
+    const glm::mat4& getView();
 
     virtual void updateUniforms(GLShader* shader) override;
+};
+
+////////////////////// CAMERA PERSPECTIVE //////////////////////////////
+
+class CameraPerspective : public CameraBase {
+public:
+    void setFOV(float fov) { m_fov = fov, m_projectionDirty = true; }
+    void setAR(float ar) { m_ar = ar, m_projectionDirty = true; }
+
+    void setPos(glm::vec3 pos) { m_pos = pos, m_viewDirty = true; }
+    void offsetPos(glm::vec3 delta_pos) { setPos(m_pos + delta_pos); }
+
+    void setAim(glm::vec3 aim) { m_aim = aim, m_viewDirty = true; }
+    void offsetAim(glm::vec3 delta_aim) { setAim(m_pos + delta_aim); }
+
+    void setUp(glm::vec3 up) { m_up = up, m_viewDirty = true; }
+
+    void setDistance(float newDistance)
+    {
+        auto offset = m_pos - m_aim;
+        auto dir = offset / glm::length(offset);
+        m_pos = m_aim + dir * newDistance;
+        m_viewDirty = true;
+    }
+
+    glm::vec3 getPos() const { return m_pos; }
+    glm::vec3 getAim() const { return m_aim; }
+    glm::vec3 getUp() const { return m_up; }
+    float getDistance() const { return glm::distance(m_pos, m_aim); }
+
+    const glm::mat4& getView();
+    const glm::mat4& getProjection();
+
+    virtual void updateUniforms(GLShader* shader) override;
+
+private:
+    glm::mat4 m_matView;
+    glm::mat4 m_matProjection;
+    glm::vec3 m_pos = { 0.f, -10.f, 3.f }, m_aim = { 0.f, 0.f, 0.f }, m_up = { 0.f, 0.f, 1.f };
+    float m_near = .1f, m_far = 2000.f;
+    float m_fov = 1.f /*radians*/, m_ar = 1.f;
+    bool m_viewDirty = true, m_projectionDirty = true;
 };
 
 #endif // ORTHOCAMERA_H
