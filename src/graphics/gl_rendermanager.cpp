6c96977d6d8b0aaecb89d8c73db85c2f3c2be47e
smarchevsky <staseg1987@gmail.com>
2023-05-08 12:36:02 +0300

    GLRenderParameters now have poly mode and static parameter

--- src/graphics/gl_rendermanager.cpp
+++ src/graphics/gl_rendermanager.cpp
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <unordered_map>
 
+GLRenderParameters GLRenderParameters::s_currentParams;
 namespace {
 void generateMipMap(GLFrameBufferBase* fb)
 {
@@ -26,7 +27,7 @@ void GLRenderManager::draw(GLShader* shader,
     frameBuffer->bind();
 
     if (clear) {
-        bool withDepth = (params.depth == GLDepth::Enabled);
+        bool withDepth = (params.depthMode == GLDepth::Enabled);
         frameBuffer->clear(withDepth);
     }
 
@@ -45,28 +46,55 @@ void GLRenderManager::draw(GLShader* shader,
 
 void GLRenderParameters::apply()
 {
-    switch (blend) {
-    case GLBlend::Disabled: {
-        glDisable(GL_BLEND);
-    } break;
-    case GLBlend::OneMinusAlpha: {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    } break;
-    default:
-        LOGE("Not supported blend: " << (int)blend);
-        assert(false);
-        break;
+    if (s_currentParams.blendMode != blendMode) {
+        s_currentParams.blendMode = blendMode;
+        switch (blendMode) {
+        case GLBlend::Disabled: {
+            glDisable(GL_BLEND);
+        } break;
+        case GLBlend::OneMinusAlpha: {
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        } break;
+        default:
+            LOGE("Not supported blend mode: " << (int)blendMode);
+            assert(false);
+            break;
+        }
     }
 
-    switch (depth) {
-    case GLDepth ::Disabled: {
-        glDisable(GL_DEPTH_TEST);
-    } break;
-    case GLDepth ::Enabled: {
-        glEnable(GL_DEPTH_TEST);
-    } break;
-    default:
-        break;
+    if (s_currentParams.depthMode != depthMode) {
+        s_currentParams.depthMode = depthMode;
+        switch (depthMode) {
+        case GLDepth ::Disabled: {
+            glDisable(GL_DEPTH_TEST);
+        } break;
+        case GLDepth ::Enabled: {
+            glEnable(GL_DEPTH_TEST);
+        } break;
+        default:
+            LOGE("Not supported depth mode: " << (int)depthMode);
+            assert(false);
+            break;
+        }
     }
+
+    if (s_currentParams.polyMode != polyMode) {
+        s_currentParams.polyMode = polyMode;
+        switch (polyMode) {
+        case GLPolyMode::Fill: {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        } break;
+        case GLPolyMode::Lines: {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        } break;
+
+        default:
+            LOGE("Not supported poly mode: " << (int)polyMode);
+            assert(false);
+            break;
+        }
+    }
+    // glEnable(GL_CULL_FACE);
+    // glCullFace(GL_BACK);
 }
