893b14329c465ef9dd9e76e65c0cabe40cb5d90b
smarchevsky <staseg1987@gmail.com>
2023-05-09 19:13:30 +0300

    Refactor texture class

--- src/graphics/gl_texture.cpp
+++ src/graphics/gl_texture.cpp
@@ -22,6 +22,7 @@ int getGLTextureFormatInternal(GLTexture2D::Format format)
         return 0;
     }
 }
+
 int getGLTextureFormatExternal(int nrChannels)
 {
     switch (nrChannels) {
@@ -35,28 +36,86 @@ int getGLTextureFormatExternal(int nrChannels)
     }
 }
 }
-GLDepthBuffer2D::GLDepthBuffer2D(glm::vec2 size)
-{
-    glGenRenderbuffers(1, &m_rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size.x, size.y);
-}
 
-GLDepthBuffer2D::~GLDepthBuffer2D()
+/////////////////// TEXTURE 2D ////////////////////
+
+bool GLTexture2D::setWrapping(Wrapping wrapping)
 {
-    if (m_rbo) {
-        glDeleteTextures(1, &m_rbo);
-        m_rbo = 0;
-        LOG("Render (depth) buffer destroyed");
+    if (!m_textureHandle) {
+        LOGE("No texture handle");
+        return false;
     }
+
+    int wrappingGLformat {};
+    switch (wrapping) {
+    case Wrapping::Repeat: {
+        wrappingGLformat = GL_REPEAT;
+    } break;
+    case Wrapping::MirrorRepeat: {
+        wrappingGLformat = GL_MIRRORED_REPEAT;
+    } break;
+    case Wrapping::ClampEdge: {
+        wrappingGLformat = GL_CLAMP_TO_EDGE;
+    } break;
+    case Wrapping::ClampBorder: {
+        wrappingGLformat = GL_CLAMP_TO_BORDER;
+    } break;
+    default:
+        LOGE("Unsuppoerted wrapping type");
+        return false;
+    }
+
+    m_wrapping = wrapping;
+    glBindTexture(GL_TEXTURE_2D, m_textureHandle);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrappingGLformat);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrappingGLformat);
+    return true;
 }
 
-GLTexture2D::~GLTexture2D()
+bool GLTexture2D::setFiltering(Filtering filtering)
 {
-    clear();
+    if (!m_textureHandle) {
+        LOGE("No texture handle");
+        return false;
+    }
+
+    int minFilter, magFilter;
+    switch (filtering) {
+    case Filtering::Nearset: {
+        minFilter = GL_NEAREST;
+        magFilter = GL_NEAREST;
+    } break;
+
+    case Filtering::NearestMipmap: {
+        minFilter = GL_NEAREST_MIPMAP_LINEAR;
+        magFilter = GL_NEAREST;
+    } break;
+
+    case Filtering::Linear: {
+        minFilter = GL_LINEAR;
+        magFilter = GL_LINEAR;
+    } break;
+
+    case Filtering::LinearMipmap: {
+        minFilter = GL_LINEAR_MIPMAP_LINEAR;
+        magFilter = GL_LINEAR;
+
+    } break;
+    default:
+        LOGE("Unsupportering texture filtering type");
+        return false;
+    }
+
+    m_filtering = filtering;
+
+    generateMipMap();
+    glBindTexture(GL_TEXTURE_2D, m_textureHandle);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+    return true;
 }
 
-bool GLTexture2D::create(glm::ivec2 size, Format format)
+bool GLTexture2D::createEmpty(glm::ivec2 size, GLTexture2D::Format format)
 {
     if (size.x <= 0 || size.y <= 0)
         return false;
@@ -104,48 +163,12 @@ bool GLTexture2D::fromImage(const Image& img)
     return true;
 }
 
-void GLTexture2D::clear()
-{
-    if (m_textureHandle) {
-        glDeleteTextures(1, &m_textureHandle);
-        m_textureHandle = 0;
-        m_size = glm::ivec2(0);
-        LOG("Texture destroyed");
-    }
-}
+// void GLTexture2D::toImage(Image& img) const
+// {
+//     // float* data = new float[m_size.x * m_size.y * 3];
+//     // glGetTextureImage(m_textureHandle, 0, GL_RGB, GL_FLOAT, m_size.x * m_size.y * 3 * 4, data);
+// }
 
-bool GLTexture2D::setWrapping(Wrapping wrapping)
-{
-    if (!m_textureHandle) {
-        LOGE("No texture handle");
-        return false;
-    }
-
-    int wrappingGLformat {};
-    switch (wrapping) {
-    case Wrapping::Repeat: {
-        wrappingGLformat = GL_REPEAT;
-    } break;
-    case Wrapping::MirrorRepeat: {
-        wrappingGLformat = GL_MIRRORED_REPEAT;
-    } break;
-    case Wrapping::ClampEdge: {
-        wrappingGLformat = GL_CLAMP_TO_EDGE;
-    } break;
-    case Wrapping::ClampBorder: {
-        wrappingGLformat = GL_CLAMP_TO_BORDER;
-    } break;
-    default:
-        LOGE("Unsuppoerted wrapping type");
-        return false;
-    }
-
-    m_wrapping = wrapping;
-    glBindTexture(GL_TEXTURE_2D, m_textureHandle);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrappingGLformat);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrappingGLformat);
-    return true;
-}
 static bool isMipMap(GLTexture2D::Filtering filtering)
 {
     return filtering == GLTexture2D::Filtering::NearestMipmap
@@ -161,45 +184,32 @@ void GLTexture2D::generateMipMap()
     }
 }
 
-bool GLTexture2D::setFiltering(Filtering filtering)
+void GLTexture2D::clear()
 {
-    if (!m_textureHandle) {
-        LOGE("No texture handle");
-        return false;
+    if (m_textureHandle) {
+        glDeleteTextures(1, &m_textureHandle);
+        m_textureHandle = 0;
+        m_size = glm::ivec2(0);
+        LOG("Texture destroyed");
     }
+}
 
-    int minFilter, magFilter;
-    switch (filtering) {
-    case Filtering::Nearset: {
-        minFilter = GL_NEAREST;
-        magFilter = GL_NEAREST;
-    } break;
+GLTexture2D::~GLTexture2D() { clear(); }
 
-    case Filtering::NearestMipmap: {
-        minFilter = GL_NEAREST_MIPMAP_LINEAR;
-        magFilter = GL_NEAREST;
-    } break;
+/////////////////// RENDER (DEPTH) BUFFER /////////////////
 
-    case Filtering::Linear: {
-        minFilter = GL_LINEAR;
-        magFilter = GL_LINEAR;
-    } break;
+GLDepthBuffer2D::GLDepthBuffer2D(glm::vec2 size)
+{
+    glGenRenderbuffers(1, &m_rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size.x, size.y);
+}
 
-    case Filtering::LinearMipmap: {
-        minFilter = GL_LINEAR_MIPMAP_LINEAR;
-        magFilter = GL_LINEAR;
-
-    } break;
-    default:
-        LOGE("Unsupportering texture filtering type");
-        return false;
+GLDepthBuffer2D::~GLDepthBuffer2D()
+{
+    if (m_rbo) {
+        glDeleteTextures(1, &m_rbo);
+        m_rbo = 0;
+        LOG("Render (depth) buffer destroyed");
     }
-
-    m_filtering = filtering;
-
-    generateMipMap();
-    glBindTexture(GL_TEXTURE_2D, m_textureHandle);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
-    return true;
 }
